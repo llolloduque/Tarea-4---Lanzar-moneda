ec19592e9910ee0005d6fd979a115ead4c6560aa
Adrian_duque <adrianduquebonilla@gmail.com>
2026-02-04 12:28:09 +0000

    Animación de moneda y retoques de archivos

--- script.js
+++ script.js
@@ -2,28 +2,44 @@ const boton = document.getElementById("jugar");
 const imagen = document.getElementById("moneda");
 const texto = document.getElementById("texto");
 
+let intervalo;
+
 boton.addEventListener("click", function () {
 
     const eleccion = document.querySelector('input[name="eleccion"]:checked');
 
     if (!eleccion) {
-        texto.textContent = "Elige cara o cruz";
+        texto.textContent = "Tienes que elegir cara o cruz";
         return;
     }
 
-    texto.textContent = "Lanzando moneda...";
-
+    texto.textContent = "Lanzando la moneda...";
+    
    
+    const duracion = Math.floor(Math.random() * 1000) + 1000;
+
+    let estado = true;
+
+    intervalo = setInterval(function () {
+        if (estado) {
+            imagen.src = "cara.png";
+        } else {
+            imagen.src = "cruz.png";
+        }
+        estado = !estado;
+    }, 200);
+
     setTimeout(function () {
+        clearInterval(intervalo);
 
         const resultado = Math.random() < 0.5 ? "cara" : "cruz";
-        imagen.src = resultado + ".png";
+        imagen.src = "" + resultado + ".png";
 
         if (resultado === eleccion.value) {
-            texto.textContent = "Has ganado. Ha salido " + resultado;
+            texto.textContent = "¡Has ganado! Ha salido " + resultado;
         } else {
             texto.textContent = "Has perdido. Ha salido " + resultado;
         }
 
-    }, 1000);
+    }, duracion);
 });
